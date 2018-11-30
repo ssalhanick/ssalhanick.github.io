fa98342e5bce8aabf0c14ff002bd722e52bc0bb6
Scott Salhanick <ssalhanick@gmail.com>
2018-11-30 00:21:27 -0600

    added slick slider

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
 
+	$('#about .slideshow').slick({
+     lazyLoad: 'ondemand',
+	  autoplay: true,
+     autoplaySpeed: 2000
+     slidesToShow: 1,
+     adaptiveHeight: true,
+	  dots: true,
+     infinite: true
+   });
+
 	function mobileNav(){
 			if (! $('nav').hasClass('active')) {
 				$('nav').addClass('active');
